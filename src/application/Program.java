0d95e84b50194c472c248dd773dec0887022e15d
Bernardo Nascimento <bernardoalves1404@gmail.com>
2021-09-10 12:29:05 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,5 +37,15 @@ public class Program {
 		Seller newSeller = new Seller(null,"Greg", "Greg@gmail.com", new Date(),4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id:" + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller Insert ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
+		
+		
 	}
+	
 }
